2e9dcaccfb78b58332726580042ea396bc1f8b78
Jeff Cardwell <jeffcardwell@gmail.com>
2016-10-26 13:46:08 -0500

    added checkForEmpty and renamed fieldCountValidator to desiredItemCountValidator

--- src/CardinalityFieldFormattersFormElementValidation.php
+++ src/CardinalityFieldFormattersFormElementValidation.php
@@ -7,8 +7,6 @@ use Drupal\Core\Form\FormStateInterface;
 class CardinalityFieldFormattersFormElementValidation {
 
 
-
-
   /**
    *
    */
@@ -19,29 +17,37 @@ class CardinalityFieldFormattersFormElementValidation {
     $lower_limit = 0;
 
     if ($this->belowLowerLimit($element_value, $lower_limit)) {
-      $form_state->setErrorByName($element_name, t('@name must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
+      $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
     }
+
+    /*if ($this->checkForEmpty($element_value)) {
+      $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
+    }*/
 
   }
 
   /**
    *
    */
-  function fieldCountValidator(array &$element, FormStateInterface &$form_state) {
+  function desiredItemCountValidator(array &$element, FormStateInterface &$form_state) {
 
     $element_name = $element['#name'];
     $element_value = $element['#value'];
     $lower_limit = 1;
 
     if ($this->belowLowerLimit($element_value, $lower_limit)) {
-      $form_state->setErrorByName($element_name, t('@name must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
+      $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
     }
+
+    /*if ($this->checkForEmpty($element_value)) {
+      $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
+    }*/
 
   }
 
-
-
   protected function belowLowerLimit($number_to_test, $lower_limit) {
+
+    //($number_to_test < $lower_limit) ? TRUE : FALSE;
 
     if ($number_to_test < $lower_limit) {
       return TRUE;
@@ -50,11 +56,21 @@ class CardinalityFieldFormattersFormElementValidation {
     return FALSE;
   }
 
-
-
   protected function belowUpperBound() {
 
   }
+
+  protected function checkForEmpty ($number_to_test) {
+
+    dpm($number_to_test);
+    if ($number_to_test == "") {
+      return TRUE;
+    }
+
+    return FALSE;
+
+  }
+
 
 
 }
