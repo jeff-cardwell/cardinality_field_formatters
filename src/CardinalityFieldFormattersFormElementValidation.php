d2b25abb4cc614a3827f347775ae9cc3bfdcf653
Jeff Cardwell <jeffcardwell@gmail.com>
2016-10-26 21:26:25 -0500

    got checkforempty when box is checked working, in the middle of adding #min to element

--- src/CardinalityFieldFormattersFormElementValidation.php
+++ src/CardinalityFieldFormattersFormElementValidation.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\cardinality_field_formatters;
 
+use Drupal\Core\Field\PluginSettingsInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\FormatterInterface;
 
 class CardinalityFieldFormattersFormElementValidation {
 
@@ -14,15 +16,24 @@ class CardinalityFieldFormattersFormElementValidation {
 
     $element_name = $element['#name'];
     $element_value = $element['#value'];
-    $lower_limit = 0;
+    $lower_limit = $element['#min'];
 
-    if ($this->belowLowerLimit($element_value, $lower_limit)) {
-      $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
+    $constrain_cardinality = $form_state->getValue(array('fields','field_test_number_field','settings_edit_form','third_party_settings','cardinality_field_formatters','constrain_cardinality'));
+
+    if ($constrain_cardinality == "1") {
+
+      if ($this->checkForBlank($element_value)) {
+        $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
+      }
+
+      if ($this->belowLowerLimit($element_value, $lower_limit)) {
+        $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array(
+          '@name' => $element['#title'],
+          '@lower_limit' => $lower_limit
+        )));
+      }
+
     }
-
-    /*if ($this->checkForEmpty($element_value)) {
-      $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
-    }*/
 
   }
 
@@ -33,44 +44,39 @@ class CardinalityFieldFormattersFormElementValidation {
 
     $element_name = $element['#name'];
     $element_value = $element['#value'];
-    $lower_limit = 1;
+    $lower_limit = $element['#min'];
 
-    if ($this->belowLowerLimit($element_value, $lower_limit)) {
-      $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array('@name' => $element['#title'], '@lower_limit' => $lower_limit)));
+    $constrain_cardinality = $form_state->getValue(array('fields','field_test_number_field','settings_edit_form','third_party_settings','cardinality_field_formatters','constrain_cardinality'));
+
+    if ($constrain_cardinality == "1") {
+      if ($this->checkForBlank($element_value)) {
+        $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
+      }
+
+      if ($this->belowLowerLimit($element_value, $lower_limit)) {
+        $form_state->setErrorByName($element_name, t('"@name" must be greater than or equal to @lower_limit.', array(
+          '@name' => $element['#title'],
+          '@lower_limit' => $lower_limit
+        )));
+      }
+
     }
-
-    /*if ($this->checkForEmpty($element_value)) {
-      $form_state->setErrorByName($element_name, t('"@name" must have a value.', array('@name' => $element['#title'])));
-    }*/
 
   }
 
   protected function belowLowerLimit($number_to_test, $lower_limit) {
 
-    //($number_to_test < $lower_limit) ? TRUE : FALSE;
-
-    if ($number_to_test < $lower_limit) {
-      return TRUE;
-    }
-
-    return FALSE;
-  }
-
-  protected function belowUpperBound() {
-
-  }
-
-  protected function checkForEmpty ($number_to_test) {
-
     dpm($number_to_test);
-    if ($number_to_test == "") {
-      return TRUE;
-    }
+    dpm($lower_limit);
 
-    return FALSE;
+    return ($number_to_test < $lower_limit) ? TRUE : FALSE;
 
   }
 
+  protected function checkForBlank ($number_to_test) {
 
+    return ($number_to_test == "") ? TRUE : FALSE;
+
+  }
 
 }
