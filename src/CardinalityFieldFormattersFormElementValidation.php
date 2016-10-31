20e0beb19037c2fec14a008b9c1d5245fa278044
Jeff Cardwell <jeffcardwell@gmail.com>
2016-10-31 12:57:09 -0500

    refactored validation funcitons, replaced custom number validator with Drupal core one, renamed some variables

--- src/CardinalityFieldFormattersFormElementValidation.php
+++ src/CardinalityFieldFormattersFormElementValidation.php
@@ -3,74 +3,60 @@
 namespace Drupal\cardinality_field_formatters;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Element\Number;
 
 class CardinalityFieldFormattersFormElementValidation {
-
-
 
   /**
    *
    */
 
-  public function validateCardinalityConstraint (array &$element, FormStateInterface &$form_state) {
+  public function validateCardinalityConstraint (array $element, FormStateInterface &$form_state, $complete_form) {
+
+    // #element that determines if we will validate
+    $element_needed_to_choose_to_validate = $element['#cardinality_field_formatters_validation_determinator'];
+
+    // value to determine if we validate
+    $element_value_needed_to_choose_to_validate = $element['#cardinality_field_formatters_validation_determinator_true_value'];
+
+    // get the array of parent indexes to current element, the element which is being validated
+    // needed for form_state->getValue()
+    $elements_parents_array = $element['#parents'];
+
+    // substitutes the final #element with the #element that determines if we will validate
+    // needed for form_state->getValue()
+    $elements_parents_array[count($elements_parents_array)-1] = $element_needed_to_choose_to_validate;
+
+    // gets form_state value of the #element that determines whether we will validate
+    $validation_determinator_value = $form_state->getValue($elements_parents_array);
+
+    if ($validation_determinator_value == $element_value_needed_to_choose_to_validate) {
+
+      self::setFormStateErrorIfBlank($element, $form_state);
+      Number::validateNumber($element, $form_state, $complete_form);
+    }
+
+  }
+
+  protected static function setFormStateErrorIfBlank(array $element, FormStateInterface &$form_state){
 
     $element_name = $element['#name'];
     $element_title = $element['#title'];
     $element_value = $element['#value'];
 
-    // minimum numeric value allowed
-    $lower_limit = $element['#min'];
-
-    // gets the index of the sibling #element that will provide the value used to
-    // determine if we will validate
-    $element_needed_to_choose_to_validate = $element['#element_needed_for_element_validate'];
-
-    // gets the necessary value to determine if we validate
-    $element_value_needed_to_choose_to_validate = $element['#element_value_needed_for_element_validate'];
-
-    // get the array of parent indexes to current element, which is the element #validate_element is called from
-    $elements_parents_array = $element['#parents'];
-
-    // uses the array of parent indexes and substitutes the final #element with the #element that is providing
-    // the value that determines whether we will validate
-    $elements_parents_array[count($elements_parents_array)-1] = $element_needed_to_choose_to_validate;
-
-    // gets value of the #element from current form_state that determines whether we will validate
-    $validation_determinator_value = $form_state->getValue($elements_parents_array);
-
-    if ($validation_determinator_value == $element_value_needed_to_choose_to_validate) {
-      if (self::valueIsBlankString($element_value)) {
-        $form_state->setErrorByName($element_name, t('"@title" must have a value.',
-          array(
+    if (self::isBlankString($element_value)) {
+      $form_state->setErrorByName($element_name, t('"@title" must have a value.',
+        array(
           '@title' => $element_title
         )));
-      }
-
-      if (self::belowLowerLimit($element_value, $lower_limit)) {
-        $form_state->setErrorByName($element_name, t('"@title" must be greater than or equal to @lower_limit.',
-          array(
-          '@title' => $element_title,
-          '@lower_limit' => $lower_limit
-        )));
-      }
-
     }
 
   }
 
-  protected static function belowLowerLimit($number_to_test, $lower_limit) {
 
-    $number_to_test = intval($number_to_test);
-    $lower_limit = intval($lower_limit);
-
-    return ($number_to_test < $lower_limit) ? TRUE : FALSE;
-
-  }
-
-  protected static function valueIsBlankString ($value_to_test) {
+  protected static function isBlankString ($value_to_test) {
 
     return ($value_to_test == "") ? TRUE : FALSE;
-
   }
 
 }
